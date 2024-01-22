abedc4c777ec6db83d2747e27a57718aa88c3e0b
Ilya Lityuga <ilya@instreamatic.com>
2024-01-22 10:59:43 +0300

    ✨ Added one more test

--- test_app.py
+++ test_app.py
@@ -10,4 +10,6 @@ def test_planetary_positions():
         "/planetary_positions?year=1990&month=9&day=5&lat=55.3948&lon=43.8399&hour=15&min=1&sec=53",
         headers={"X-Token": "coneofsilence"},
     )
+    response_json = response.json()
     assert response.status_code == 200
+    assert response_json['success'] == 1
